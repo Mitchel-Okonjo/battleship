c04bc465e8005c44fd8dff94f1490a7a087e8020
Mitchel-Okonjo <111641404+Mitchel-Okonjo@users.noreply.github.com>
2023-10-05 21:51:36 +0100

    Jest config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,29 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: ["node_modules"],
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      config$: "./configs/app-config.js",
+      react: "./vendor/react-master",
+    },
+    extensions: [".js", ".jsx"],
+    modules: [
+      "node_modules",
+      "bower_components",
+      "shared",
+      "/shared/vendor/modules",
+    ],
+  },
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
