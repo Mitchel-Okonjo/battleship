2c54cf6ee515f1b95cbfdb10c6fa2ec4afc03827
Mitchel-Okonjo <111641404+Mitchel-Okonjo@users.noreply.github.com>
2023-10-06 22:34:08 +0100

    Create and test gameboard functions

--- src/all.test.js
+++ src/all.test.js
@@ -1,12 +1,52 @@
 const { Ship } = require("./ship");
+const { Gameboard } = require("./gameboard");
 
 const titan = new Ship(3);
 titan.hits = 3;
+const gameBoard = new Gameboard();
 
-test("check if ship has been sunk", () => {
+test("Correctly determine if ship has sunk", () => {
   expect(titan.isSunk()).toBe(true);
 });
 
-test("check that ship hits are being updated", () => {
+test("Correctly update ship hits", () => {
   expect(titan.hit()).toBe(4);
+});
+
+test("Correctly creates 10 by 10 array", () => {
+  expect(gameBoard.arr[9][9]).toBe(0);
+  gameBoard.arr[0][0] = 1;
+  expect(gameBoard.arr[0][0]).toBe(1);
+});
+
+test("Correctly identifies successful attack", () => {
+  gameBoard.arr[9][9] = titan;
+  gameBoard.receiveAttack(9, 9);
+  expect(titan.hits).toBe(5);
+  expect(gameBoard.arr[9][9]).toBeNull();
+});
+
+test("Correctly identifies Missed shots", () => {
+  gameBoard.miss(8, 9);
+  expect(gameBoard.arr[8][9]).toBe(1);
+});
+
+test("Correclty places ships ", () => {
+  gameBoard.placeShip(1, 0, "y", 2);
+  expect(gameBoard.arr[1][0]).toEqual(new Ship(2));
+  expect(gameBoard.arr[1][1]).toBe(0);
+  expect(gameBoard.arr[2][0]).toEqual(gameBoard.arr[1][0]);
+  expect(gameBoard.arr[3][0]).toBe(0);
+});
+
+test("Corrently determine if all ships are sunk", () => {
+  const spartan = new Ship(3);
+  const spartan2 = new Ship(2);
+  spartan.hits = 3;
+  gameBoard.ships = [];
+  gameBoard.ships.push(spartan);
+  gameBoard.ships.push(spartan2);
+  expect(gameBoard.allShipsSunk()).toBeFalsy();
+  gameBoard.ships[1].hits = 2;
+  expect(gameBoard.allShipsSunk()).toBeTruthy();
 });
---
+++ src/gameboard.js
@@ -0,0 +1,62 @@
+const { Ship } = require("./ship");
+
+class Gameboard {
+  constructor() {
+    this.arr = createArray();
+    this.ships = [];
+  }
+
+  receiveAttack(y, x) {
+    if (this.arr[y][x] instanceof Ship) {
+      this.arr[y][x].hit();
+      this.arr[y][x] = null;
+    } else {
+      this.miss(y, x);
+    }
+    return this.arr;
+  }
+
+  miss(y, x) {
+    this.arr[y][x] = 1;
+    return this.arr;
+  }
+
+  allShipsSunk() {
+    let i = 0;
+    while (i < this.ships.length) {
+      if (!this.ships[i].isSunk()) {
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
+
+  placeShip(y, x, axis, length, ship = new Ship(length)) {
+    let i = 0;
+    let p = axis === "y" ? y : x;
+
+    while (i < ship.length) {
+      if (axis === "y") {
+        this.arr[p][x] = ship;
+      } else {
+        this.arr[y][p] = ship;
+      }
+      p++;
+      i++;
+    }
+    this.ships.push(ship);
+  }
+}
+
+function createArray() {
+  const arr = new Array(10);
+
+  for (let i = 0; i < 10; i++) {
+    arr[i] = new Array(10).fill(0);
+  }
+
+  return arr;
+}
+
+module.exports = { Gameboard };
